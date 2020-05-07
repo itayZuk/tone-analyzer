ec907b786b7cf454e785afe6ecdb54d184f5c9a6
Itay <it.zukman@gmail.com>
2020-05-07 23:27:33 +0300

    [add] - graph.py to draw graphs

---
+++ graph.py
@@ -0,0 +1,14 @@
+"""
+A graph drawer
+"""
+from matplotlib import pyplot
+
+
+def main():
+    pyplot.plot([1, 2, 3, 4])
+    pyplot.ylabel('some numbers')
+    pyplot.show()
+
+
+if __name__ == '__main__':
+    main()
